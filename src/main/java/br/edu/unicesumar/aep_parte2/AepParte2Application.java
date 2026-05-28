d6700f89cbff584bf372e1a1510a1426e5dfd5a4
Rafaelbrk75 <rafaelbrk75@gmail.com>
2026-05-28 19:58:20 -0300

    Primeiro commit

---
+++ src/main/java/br/edu/unicesumar/aep_parte2/AepParte2Application.java
@@ -0,0 +1,13 @@
+package br.edu.unicesumar.aep_parte2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AepParte2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AepParte2Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/unicesumar/aep_parte2/AepParte2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unicesumar.aep_parte2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AepParte2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
