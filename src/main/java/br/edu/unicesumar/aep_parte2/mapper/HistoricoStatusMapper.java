b88a1a8dca9e2c8d2b25c25790b0e8224809daa4
Rafaelbrk75 <rafaelbrk75@gmail.com>
2026-05-31 20:46:56 -0300

    Mapper implementado

--- src/main/java/br/edu/unicesumar/aep_parte2/mapper/HistoricoStatusMapper.java
+++ src/main/java/br/edu/unicesumar/aep_parte2/mapper/HistoricoStatusMapper.java
@@ -1,4 +1,17 @@
 package br.edu.unicesumar.aep_parte2.mapper;
 
-public class HistoricoStatusMapper {
+import br.edu.unicesumar.aep_parte2.domain.dto.HistoricoStatusResponse;
+import br.edu.unicesumar.aep_parte2.domain.entity.HistoricoStatusModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface HistoricoStatusMapper {
+
+    @Mapping(source = "atendenteModel.nome", target = "nomeAtendente")
+    HistoricoStatusResponse toResponse(HistoricoStatusModel model);
+
+    List<HistoricoStatusResponse> toResponseList(List<HistoricoStatusModel> models);
 }
--- src/main/java/br/edu/unicesumar/aep_parte2/mapper/SolicitacaoMapper.java
+++ src/main/java/br/edu/unicesumar/aep_parte2/mapper/SolicitacaoMapper.java
@@ -1,4 +1,29 @@
 package br.edu.unicesumar.aep_parte2.mapper;
 
-public class SolicitacaoMapper {
+import br.edu.unicesumar.aep_parte2.domain.dto.SolicitacaoRequest;
+import br.edu.unicesumar.aep_parte2.domain.dto.SolicitacaoResponse;
+import br.edu.unicesumar.aep_parte2.domain.entity.SolicitacaoModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface SolicitacaoMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "protocolo", ignore = true)
+    @Mapping(target = "dataCriacao", ignore = true)
+    @Mapping(target = "atrasado", ignore = true)
+    @Mapping(target = "solicitante", ignore = true)
+    @Mapping(target = "historico", ignore = true)
+    @Mapping(target = "statusAtual", ignore = true)
+    SolicitacaoModel toEntity(SolicitacaoRequest request);
+
+    // Entity → Response
+    // campos com nomes diferentes precisam de @Mapping explícito
+    @Mapping(source = "solicitante.nome", target = "nomeSolicitante")
+    SolicitacaoResponse toResponse(SolicitacaoModel model);
+
+    List<SolicitacaoResponse> toResponseList(List<SolicitacaoModel> models);
 }
